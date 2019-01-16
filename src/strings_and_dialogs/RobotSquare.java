bbacba5559cd96317bbb892f8b853ee2217192cf
codeclub <codeclub@smms-rm23-24.smusd.local>
2019-01-16 14:55:59 -0800

    robot square

--- src/strings_and_dialogs/RobotSquare.java
+++ src/strings_and_dialogs/RobotSquare.java
@@ -12,9 +12,46 @@ public class RobotSquare {
     public static void main(String[] args) throws Exception {
  
     	// 1. Make a new Robot
-
-
+Robot q=new Robot("batman");
+    	
         // 3. Put the robot's pen down
+q.moveTo(350, 400);
+q.penDown();
+q.setSpeed(200);
+q.setPenWidth(100000000);
+for (int i = 0; i < 4; i++) {
+	q.setRandomPenColor();
+q.move(200);
+q.turn(90);	
+}
+q.moveTo(340, 390);
+for (int i = 0; i < 4; i++) {
+	q.setRandomPenColor();
+q.move(200);
+q.turn(90);	
+}
+
+q.moveTo(330, 380);
+for (int i = 0; i < 4; i++) {
+	q.setRandomPenColor();
+q.move(200);
+q.turn(90);	
+}
+
+q.moveTo(320, 370);
+for (int i = 0; i < 4; i++) {
+	q.setRandomPenColor();
+q.move(200);
+q.turn(90);	
+}
+
+q.moveTo(310, 360);
+for (int i = 0; i < 4; i++) {
+	q.setRandomPenColor();
+q.move(200);
+q.turn(90);	
+}
+
 
 
         // 6. Make the robot move as fast as possible
